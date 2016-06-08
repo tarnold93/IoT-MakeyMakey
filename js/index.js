b0be02cf4bdd0f25458797de8bd6f7c044c6a343
tarnold93 <tarnold@engr-131-27.eastern.ewu.edu>
2016-06-08 14:59:14 -0700

    Initial Commit
    Did all changes on Codepen and then uploaded to GitHub.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$('html').keypress( function(e){
+  if (e.keyCode === 32) {
+   $('body').addClass('newImg');
+   $('h1').text("We would do well to slow down a little, proceed at the optimum speed for our circumstances, focus on the significant, lift up our eyes, and truly see the things that matter most.");
+   $( "p" ).remove( ":contains('-Dieter F. Uchtdorf')" );
+   $('body').append("<p>-Dieter F. Uchtdorf</p>");
+  }
+});
